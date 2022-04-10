ed6c7fbe0052a8f41e5fae573b0bd33240a1ac69
Bartosz Leper <bl.nero@gmail.com>
2022-04-10 14:03:55 +0200

    Connect to external Rust binary and remove the Hello World command

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,21 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Activating Steampunk 6502 debugger');
 
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "steampunk-6502-debugger" is now active!');
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('steampunk-6502-debugger.helloWorld', () => {
-    // The code you place here will be executed every time your command is executed
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World from Steampunk 6502 Debugger!');
-  });
-
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.debug.registerDebugAdapterDescriptorFactory(
+      'steampunk-6502',
+      new SteampunkDebugAdapterDescriptorFactory()));
 }
 
-// this method is called when your extension is deactivated
+class SteampunkDebugAdapterDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
+  createDebugAdapterDescriptor(
+    session: vscode.DebugSession,
+    executable: vscode.DebugAdapterExecutable | undefined
+  ): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
+    return new vscode.DebugAdapterServer(1234);
+  }
+}
+
 export function deactivate() { }
